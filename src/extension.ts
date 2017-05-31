e86332333378815e781d14ae90bd5e587daa9240
kubosho <ta2@o2p.jp>
2017-05-31 21:06:26 +0900

    docs(src): remove comments

--- src/extension.ts
+++ src/extension.ts
@@ -19,13 +19,7 @@ function changeColorTheme() {
         });
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = commands.registerCommand('extension.whimsy', () => {
         setTimeout(changeColorTheme, 1000 * 60 * 30);
     });
@@ -34,6 +28,5 @@ export function activate(context: ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
