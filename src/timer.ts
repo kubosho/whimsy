25cf4c091f374315f7f992147bbcee9cf32279d3
kubosho <ta2@o2p.jp>
2017-06-02 22:22:20 +0900

    feat(src): remove unnecessary function

--- src/timer.ts
+++ src/timer.ts
@@ -1,28 +1,17 @@
 export class Timer {
     private _id: NodeJS.Timer;
-
-    private _delay = 1000 * 60 * 30;
-    private _interval: number;
-
-    set delay(milliseconds: number) {
-        this._delay = milliseconds;
-    }
+    private _interval = 1000 * 60 * 30;
 
     set interval(milliseconds: number) {
         this._interval = milliseconds;
     }
 
     start(callback: () => void): void {
-        if (this._interval) {
-            this._id = setInterval(callback, this._interval);
-            return;
-        }
-
-        this._id = setTimeout(callback, this._delay);
+        this._id = setInterval(callback, this._interval);
     }
 
     stop(): void {
-        this._interval ? clearInterval(this._id) : clearTimeout(this._id);
+        clearInterval(this._id);
         this._id = null;
     }
 }
