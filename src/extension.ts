d530bc42c7efa3903dd494cab69c2630cd4d451a
kubosho <ta2@o2p.jp>
2017-06-04 18:45:08 +0900

    refactor(src): use async/await

--- src/extension.ts
+++ src/extension.ts
@@ -18,37 +18,33 @@ export function activate(context: ExtensionContext) {
         });
     });
 
-    const updateInterval = commands.registerCommand('extension.updateInterval', () => {
+    const updateInterval = commands.registerCommand('extension.updateInterval', async () => {
         const option = {
             prompt: 'Enter the time interval for change color theme. The unit is milliseconds.',
         };
+        const interval: string = await window.showInputBox(option)
+        const intervalValue = Number(interval);
 
-        window.showInputBox(option)
-            .then((interval: string) => {
-                const intervalValue = Number(interval);
-                if (Number.isNaN(intervalValue)) {
-                    return;
-                }
+        if (Number.isNaN(intervalValue)) {
+            return;
+        }
 
-                timer.stop();
-                timer.interval = intervalValue;
-                timer.start(() => {
-                    colorTheme.update(colorTheme.rand());
-                });
-            });
+        timer.stop();
+        timer.interval = intervalValue;
+        timer.start(() => {
+            colorTheme.update(colorTheme.rand());
+        });
     });
 
-    const updateThemeList = commands.registerCommand('extension.updateThemeList', () => {
+    const updateThemeList = commands.registerCommand('extension.updateThemeList', async () => {
         const option = {
             prompt: 'Enter theme names separated with commas.',
         };
+        const themes: string = await window.showInputBox(option);
 
-        window.showInputBox(option)
-            .then((themes: string) => {
-                colorTheme.themes = themes
-                    .split(',')
-                    .map((theme: string) => theme.trim());
-            });
+        colorTheme.themes = themes
+            .split(',')
+            .map((theme: string) => theme.trim());
     });
 
     context.subscriptions.push(
