ce5bf7a67bff4fb9ab4ca64d34eb8cfd74e8a2d7
kubosho <ta2@o2p.jp>
2017-06-08 00:18:58 +0900

    feat(src): implement of convert to millisecondes

---
+++ src/utils/convert-milliseconds.ts
@@ -0,0 +1,42 @@
+function splitNumberAndUnit(input: string): { value: number, unit: string } {
+    const re = /(\d*\.)?(\d*)(ms|s|m|h)/
+    // ["100ms", undefined, "100", "ms", index: 0, input: "1000ms"]
+    // ["1.5s", "1.", "5", "s", index: 0, input: "1.5ms"]
+    const splitedValue = input.match(re)
+
+    if (!splitedValue) {
+        return
+    }
+
+    const value = !splitedValue[1]
+        ? splitedValue[2]
+        : `${splitedValue[1]}${splitedValue[2]}`
+    const unit = splitedValue[3]
+
+    return {
+        value: Number(value),
+        unit,
+    }
+}
+
+export default function convertMilliseconds(target: string): number {
+    const numberAndUnit = splitNumberAndUnit(target)
+
+    if (!(numberAndUnit && numberAndUnit.unit)) {
+        throw new TypeError('The value is unexpected. Expect a value with unit.')
+    }
+
+    if (numberAndUnit.unit === 's') {
+        return numberAndUnit.value * 1000
+    }
+
+    if (numberAndUnit.unit === 'm') {
+        return numberAndUnit.value * 1000 * 60
+    }
+
+    if (numberAndUnit.unit === 'h') {
+        return numberAndUnit.value * 1000 * 60 * 60
+    }
+
+    return numberAndUnit.value
+}
